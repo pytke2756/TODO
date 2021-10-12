bed5efa1496483d50926e9299db6a005c0930eba
test <test@gmal.com>
2021-10-12 16:15:25 +0200

    Alap lista felvétel

---
+++ main.js
@@ -0,0 +1,27 @@
+let index = 0;
+function felvesz() {
+    let inputData = document.getElementById("bevitel").value;
+    let sor = document.createElement("li");
+    let checkbox = document.createElement("input");
+    let x = document.createElement("span");
+    x.append("X");
+    x.className = "torles";
+    x.setAttribute("data-id", index);
+
+    checkbox.type = "checkbox";
+    sor.appendChild(checkbox);
+    sor.append(inputData);
+    sor.appendChild(x);
+
+    document.getElementById("todo_data").appendChild(sor);
+
+    document.getElementById("bevitel").value = "";
+    index++;
+}
+
+
+function init() {
+    document.getElementById("bevitel_gomb").addEventListener("click", felvesz);
+}
+
+document.addEventListener("DOMContentLoaded", init);
