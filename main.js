b03c9305a5d4599affb11559b9debcaff69b8236
Ibrányi Levente <ibranyilevente@gmail.com>
2021-10-13 19:31:08 +0200

    add torol, athoz

--- main.js
+++ main.js
@@ -5,11 +5,12 @@ function felvesz() {
     let sor = document.createElement("li");
     let checkbox = document.createElement("input");
     let x = document.createElement("span");
-    x.append("X");
+    x.append(" X");
     x.className = "torles";
-    x.setAttribute("data-id", index);
+    x.addEventListener("click", torol);
 
     checkbox.type = "checkbox";
+    checkbox.addEventListener("change", athuz);
     sor.appendChild(checkbox);
     sor.append(inputData);
     sor.appendChild(x);
@@ -20,12 +21,24 @@ function felvesz() {
     index++;
 }
 
+function torol(e) {
+    e.target.parentElement.remove();
+}
+
+function athuz(e) {
+    if(e.target.checked){
+        e.target.parentElement.style.textDecoration="line-through";
+    }else{
+        e.target.parentElement.style.textDecoration="none";
+    }
+}
+
 
 function init() {
     document.getElementById("bevitel_gomb").addEventListener("click", felvesz);
-    document.getElementById("todo_data").addEventListener('click', function(e) {
+    /*document.getElementById("todo_data").addEventListener('click', function(e) {
         this.removeChild(e.target);
-      })
+      })*/
 }
 
 document.addEventListener("DOMContentLoaded", init);
